18ded74633839716180ff44a3ce5fc82cf5c466f
dograkunal <kunaldogra31@gmail.com>
2023-11-28 21:10:50 +0530

    PWA created

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const gstRateArr = [0.25, 3, 12, 18, 28];
+  const saleType = ["Local Sale", "Inter-State Sale"];
+
   const [value, setValue] = useState("");
-  const [result, setResult] = useState(null);
   const [selectedGstRate, setSelectedGstRate] = useState(gstRateArr[0]);
+  const [selectedSaleType, setselectedSaleType] = useState(saleType[0]);
+  const [originalAmount, setOriginalAmount] = useState(null);
+  const [gstAmount, setGstAmount] = useState(null);
+  const [roundoff, setRoundOff] = useState({});
+  const [submitted, setSubmitted] = useState(false);
+
+  const roundOffWithSign = (number) => {
+    const factor = Math.pow(10, 2);
+    const rounded = Math.round(number * factor) / factor;
+    const difference = rounded - number;
+
+    return {
+      roundedAmount: rounded.toFixed(2),
+      sign: difference >= 0 ? "+" : "-",
+      difference: Math.abs(difference).toFixed(2),
+    };
+  };
+
+  useEffect(() => {
+    if (submitted) {
+      const original = Number(value / (1 + selectedGstRate / 100)).toFixed(2);
+      setOriginalAmount(original);
+
+      const gst = (original * (selectedGstRate / 100)).toFixed(2);
+      setGstAmount(gst);
+
+      const roundOffResult = roundOffWithSign(
+        original + parseFloat(gst),
+        value
+      );
+      setRoundOff(roundOffResult);
+    }
+  }, [submitted, value, selectedGstRate]);
 
   const calculateValue = () => {
     if (!value || isNaN(value)) {
       alert("Please enter a valid numeric value.");
       return;
     }
-    // const cgstValue = Number((value * 9) / 100).toFixed(2);
-    // const sgstValue = Number((value * 9) / 100).toFixed(2);
-    const gstValue = Number((value * selectedGstRate) / 100).toFixed(2);
 
-    const finalAmount = Number(gstValue) + Number(value);
-    setResult(finalAmount.toFixed(2));
+    setOriginalAmount(null);
+    setGstAmount(null);
+    setRoundOff({});
+    setSubmitted(true);
   };
 
   const handleRadioChange = (event) => {
     setSelectedGstRate(parseFloat(event.target.value));
   };
-  const cgstValue = (selectedGstRate / 2).toFixed(2);
+
+  const handleTypeChange = (event) => {
+    setselectedSaleType(event.target.value);
+  };
+
+  const cgstPercentage = (selectedGstRate / 2).toFixed(2);
+  const cgstAmount = (gstAmount / 2).toFixed(2);
 
   return (
     <section className="baseContainer">
       <div className="headerBase">
-        <h1>GST Calculator</h1>
+        <h1>GST Sale Calculator</h1>
       </div>
       <div className="radioButton">
         <h2 className="rateHead">Please select GST Rate :</h2>
@@ -44,6 +83,20 @@ function App() {
           </label>
         ))}
       </div>
+      <div className="radioButton">
+        <h2 className="rateHead">Please select sale Type :</h2>
+        {saleType.map((el) => (
+          <label key={el} className="rateValue">
+            <input
+              type="radio"
+              value={el}
+              checked={selectedSaleType === el}
+              onChange={handleTypeChange}
+            />
+            {`${el}`}
+          </label>
+        ))}
+      </div>
       <div className="amountContainer">
         <input
           placeholder="Amount"
@@ -56,17 +109,28 @@ function App() {
           Submit
         </button>
       </div>
-      <div>
-        {result !== null && (
-          <div className="resultContainer">
-            <h4>Final amount: &#x20B9;{result}</h4>
+      {submitted && (
+        <div className="resultContainer">
+          <h3>Sale amount: &#x20B9;{value}</h3>
+          <h3>Taxable Value: &#x20B9;{originalAmount}</h3>
+          <div className="taxDetailContainer">
+            <i className="taxLabel">Total GST: {`${gstAmount}`}</i>
             <i className="infoLabel">
-              Including : {`${cgstValue}%`} of CGST and {`${cgstValue}%`} of
-              SGST
+              CGST: {`@ ${cgstPercentage}% on`}{" "}
+              {selectedSaleType === saleType[0] ? `${cgstAmount}` : "NIL"}
             </i>
+            <i className="infoLabel">
+              SGST: {`@ ${cgstPercentage}% on`}{" "}
+              {selectedSaleType === saleType[0] ? `${cgstAmount}` : "NIL"}
+            </i>
+            <i className="infoLabel">
+              IGST: {`@ ${selectedGstRate}% on`}{" "}
+              {selectedSaleType === saleType[1] ? `${gstAmount}` : "NIL"}
+            </i>
+            <p>RoundOff: {`${roundoff.sign} ${roundoff.difference}`} </p>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </section>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Service Worker registration failed:", error);
+    });
+}
